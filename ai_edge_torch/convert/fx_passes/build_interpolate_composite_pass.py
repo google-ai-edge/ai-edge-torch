a230e0cb542023f7f2dbe9af99104ec09e4ca57b
Chunnien Chan <121328115+chunnienc@users.noreply.github.com>
2024-06-21 10:07:58 -0700

    Add comments

--- ai_edge_torch/convert/fx_passes/build_interpolate_composite_pass.py
+++ ai_edge_torch/convert/fx_passes/build_interpolate_composite_pass.py
@@ -21,7 +21,12 @@ from ai_edge_torch.convert.fx_passes._pass_base import ExportedProgramPassBase
 from ai_edge_torch.convert.fx_passes._pass_base import ExportedProgramPassResult  # NOQA
 from ai_edge_torch.hlfb import mark_pattern
 
-_interpolate_decompositions = torch._decomp.get_decompositions(
+# For torch nightly released after mid June 2024,
+# torch.nn.functional.interpolate no longer gets exported into decomposed graph
+# but single aten op torch.ops.aten.upsample_nearest2d.vec/torch.ops.aten.upsample_bilinear2d.vec.
+# This behavior would our pattern matching based composite builder.
+# It requires the pattern and model graph to get decomposed first for backward compatibility.
+_INTERPOLATE_DECOMPOSITIONS = torch._decomp.get_decompositions(
     [
         torch.ops.aten.upsample_bilinear2d.vec,
         torch.ops.aten.upsample_nearest2d.vec,
@@ -37,7 +42,7 @@ def _get_upsample_bilinear2d_pattern():
           x, scale_factor=2, mode="bilinear", align_corners=False
       ),
       export_args=(torch.rand(1, 3, 100, 100),),
-      decomp_table=_interpolate_decompositions,
+      decomp_table=_INTERPOLATE_DECOMPOSITIONS,
   )
 
   @pattern.register_attr_builder
@@ -60,7 +65,7 @@ def _get_upsample_bilinear2d_align_corners_pattern():
           x, scale_factor=2, mode="bilinear", align_corners=True
       ),
       export_args=(torch.rand(1, 3, 100, 100),),
-      decomp_table=_interpolate_decompositions,
+      decomp_table=_INTERPOLATE_DECOMPOSITIONS,
   )
 
   @pattern.register_attr_builder
@@ -81,7 +86,7 @@ def _get_interpolate_nearest2d_pattern():
       "tfl.resize_nearest_neighbor",
       lambda x: torch.nn.functional.interpolate(x, scale_factor=2, mode="nearest"),
       export_args=(torch.rand(1, 3, 100, 100),),
-      decomp_table=_interpolate_decompositions,
+      decomp_table=_INTERPOLATE_DECOMPOSITIONS,
   )
 
   @pattern.register_attr_builder
@@ -107,8 +112,7 @@ class BuildInterpolateCompositePass(ExportedProgramPassBase):
     ]
 
   def call(self, exported_program: torch.export.ExportedProgram):
-    # Forward compatibility
-    exported_program = exported_program.run_decompositions(_interpolate_decompositions)
+    exported_program = exported_program.run_decompositions(_INTERPOLATE_DECOMPOSITIONS)
 
     graph_module = exported_program.graph_module
     for pattern in self._patterns:
