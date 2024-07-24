cb1e13355fbcd147d144e0ffe7a64b3744d14855
Yi-Chun Kuo <yichunkuo@google.com>
2024-07-24 09:11:22 -0700

    update

--- ai_edge_torch/generative/layers/unet/blocks_2d.py
+++ ai_edge_torch/generative/layers/unet/blocks_2d.py
@@ -220,10 +220,9 @@ class FeedForwardBlock2D(nn.Module):
   def forward(self, input_tensor: torch.Tensor) -> torch.Tensor:
     residual = input_tensor
     B, C, H, W = input_tensor.shape
-    x = input_tensor
     if self.config.normalization_config.type == layers_cfg.NormalizationType.GROUP_NORM:
-      x = self.norm(x)
-      x = input_tensor.view(B, C, H * W)
+      x = self.norm(input_tensor)
+      x = x.view(B, C, H * W)
       x = x.transpose(-1, -2)
     else:
       x = input_tensor.view(B, C, H * W)
