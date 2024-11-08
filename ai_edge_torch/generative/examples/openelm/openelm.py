a2522cf24d75eef1f5fe55e790deec307e4774c5
Google AI Edge <ai-edge-bot@google.com>
2024-11-08 11:16:46 -0800

    Enable HLFB in OpenELM
    PiperOrigin-RevId: 694573151

--- ai_edge_torch/generative/examples/openelm/openelm.py
+++ ai_edge_torch/generative/examples/openelm/openelm.py
@@ -93,6 +93,7 @@ def get_model_config(kv_cache_max_len: int = 1024) -> cfg.ModelConfig:
       kv_cache_max_len=kv_cache_max_len,
       block_configs=[get_block_config(i) for i in range(num_layers)],
       final_norm_config=norm_config,
+      enable_hlfb=True,
   )
   return config
 
