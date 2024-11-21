4872c15c0aa48d5669cb426abab79351651d1133
T.J. Alumbaugh <talumbau@google.com>
2024-11-21 12:57:58 -0800

    Add inline RoPE utility function
    PiperOrigin-RevId: 698886340

--- ai_edge_torch/generative/layers/rotary_position_embedding.py
+++ ai_edge_torch/generative/layers/rotary_position_embedding.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 # ==============================================================================
 # Implementation for Rotary Position embedding. https://arxiv.org/pdf/2104.09864.pdf
+from typing import Tuple
 import torch
 
 
@@ -36,3 +37,52 @@ def apply_rope(
   rotated = torch.cat((-x2, x1), dim=-1)  # (B, nh, T, hs)
   roped = (x * cos) + (rotated * sin)
   return roped.transpose(1, 2).type_as(x)
+
+
+def apply_rope_inline(
+    q: torch.Tensor,
+    k: torch.Tensor,
+    input_pos: torch.Tensor,
+    n_elem: int,
+    base: int = 10_000,
+) -> Tuple[torch.Tensor, torch.Tensor]:
+  """Computes rotary positional embedding inline for a query and key.
+
+  Args:
+    q: the query tensor.
+    k: the key tensor.
+    input_pos: the sequence indices for the query and key
+    n_elem: number of elements of the head dimension for RoPE computation
+
+  Returns:
+    output the RoPE'd query and key.
+  """
+
+  if n_elem <= 0:
+    return q, k
+
+  theta = 1.0 / (base ** (torch.arange(0, n_elem, 2).float() / n_elem))
+  freq_exponents = (2.0 / n_elem) * torch.arange(
+      q.shape[-1] // 2, dtype=torch.float32
+  )
+  timescale = float(base) ** freq_exponents
+  radians = input_pos.clone().unsqueeze(0).unsqueeze(-1) / timescale.unsqueeze(
+      0
+  ).unsqueeze(0)
+  cos = torch.cos(radians).type_as(q)
+  sin = torch.sin(radians).type_as(q)
+
+  def apply(x, sin, cos):
+    x = x.transpose(1, 2)
+    b, h, s, d = x.shape
+    ans = torch.split(x, d // 2, dim=-1)
+    x1, x2 = ans
+    left = x1 * cos - x2 * sin
+    right = x2 * cos + x1 * sin
+    res = torch.cat([left, right], dim=-1)
+    res = res.transpose(1, 2)
+    return res
+
+  q_roped = apply(q, sin, cos)
+  k_roped = apply(k, sin, cos)
+  return q_roped, k_roped
