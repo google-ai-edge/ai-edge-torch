07e0abfeb108e89d9052425e0eb624827a327f99
Chun-nien Chan <cnchan@google.com>
2025-03-14 10:41:37 -0700

    add tfl.slice dd dynamism impl
    PiperOrigin-RevId: 736913125

--- ai_edge_torch/odml_torch/experimental/torch_tfl/_ops.py
+++ ai_edge_torch/odml_torch/experimental/torch_tfl/_ops.py
@@ -13,6 +13,8 @@
 # limitations under the License.
 # ==============================================================================
 """Torch-TFL op definitions and fake implementations."""
+import re
+from typing import Sequence
 from ai_edge_torch.odml_torch.experimental.torch_tfl import torch_library_utils
 import torch
 
@@ -30,3 +32,54 @@ def tfl_batch_matmul(
   if adj_y:
     y = torch.transpose(y, -1, -2)
   return torch.matmul(x, y)
+
+
+@custom_op_with_fake("tfl::slice")
+def tfl_slice(
+    input: torch.Tensor, begin: Sequence[int], size: Sequence[int]
+) -> torch.Tensor:
+  assert len(begin) == len(size) == input.ndim
+
+  slices = [slice(i, i + l) for i, l in zip(begin, size)]
+  return input[tuple(slices)].clone()
+
+
+@torch.library.custom_op("tfl::slice.tensor", mutates_args=())
+def tfl_slice_tensor(
+    input: torch.Tensor,
+    begin: torch.Tensor,
+    size: torch.Tensor,
+    *,
+    shape: str = "",
+) -> torch.Tensor:
+  assert begin.ndim == size.ndim == 1
+  assert begin.numel() == size.numel() == input.ndim
+  assert begin.dtype == torch.int32 and size.dtype == torch.int32
+  assert not shape or shape.count(",") == input.ndim - 1
+
+  slices = [slice(i, i + l) for i, l in zip(begin.tolist(), size.tolist())]
+  return input[tuple(slices)].clone()
+
+
+@torch.library.register_fake("tfl::slice.tensor")
+def tfl_slice_tensor_fake(
+    input: torch.Tensor,
+    begin: torch.Tensor,
+    size: torch.Tensor,
+    *,
+    shape: str = "",
+) -> torch.Tensor:
+  ctx = torch.library.get_ctx()
+  shape_str = shape
+  if not shape_str:
+    shape_str = ",".join(["?" for _ in range(input.ndim)])
+
+  shape = []
+  shape_symbols = shape_str.split(",")
+  for sym in shape_symbols:
+    if re.match(r"\d+", sym):
+      shape.append(int(sym))
+    else:
+      nnz = ctx.new_dynamic_size()
+      shape.append(nnz)
+  return input.new_empty(shape, dtype=input.dtype)
